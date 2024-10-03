13db483c927b40876acd0032f583c63cb1a1b967
Abner do Nascimento <abnerdonantos06@gmail.com>
2024-10-02 22:00:11 -0300

    feat: add validation to form inputs and local storage

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
-console.log('testando')
+const form = document.getElementById('form');
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+const nameRegex = /^[A-Za-z\s]+$/;
+const fields = document.querySelectorAll(".required");
+const validate = document.querySelectorAll(".validate");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const validName = nameValidate();
+    const validEmail = emailValidate();
+    const validMessage = messageValidate();
+
+    if (validName && validEmail && validMessage){
+        const listLocal = JSON.parse(localStorage.getItem('listLocal') || '[]')
+        listLocal.push({
+            name: fields[0].value,
+            email: fields[1].value,
+            message: fields[2].value
+        })
+
+        localStorage.setItem('listLocal', JSON.stringify(listLocal))
+        
+        fields[0].value = '';
+        fields[1].value = '';
+        fields[2].value = '';
+    }
+})
+
+function validateError(i){
+    fields[i].classList.add('border-error');
+    validate[i].style.display = 'block';
+}
+
+function validateApproved(i){
+    fields[i].classList.remove('border-error');
+    validate[i].style.display = 'none';
+}
+
+function nameValidate(){
+    if (fields[0].value === "" || !nameRegex.test(fields[0].value)) {
+        validateError(0);
+        return false;
+    } else {
+        validateApproved(0);
+        return true;
+    }
+}
+
+function emailValidate(){
+    if(emailRegex.test(fields[1].value)){
+        validateApproved(1);
+        return true;
+    }else{
+        validateError(1);
+        return false;
+    }
+}
+
+function messageValidate(){
+    if(fields[2].value.length<10){
+        validateError(2);
+        return false;
+    }else{
+        validateApproved(2);
+        return true;
+    }
+}
